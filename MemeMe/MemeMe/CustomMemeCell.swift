31cc3f62d41f4d5c60d5f0c9654165fcff906dd8
fredewiik <1988.fred@gmail.com>
2015-04-10 16:16:43 +0200

    Committed by Udacity

---
+++ MemeMe/MemeMe/CustomMemeCell.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  MemeMe
+//
+//  Created by Frédéric Lépy on 09/04/2015.
+//  Copyright (c) 2015 Frédéric Lépy. All rights reserved.
+//
+
+import UIKit
+
+class CustomMemeCell : UICollectionViewCell {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+}
